f392a3624b5988c9ca8a0a277b51c602648a38b1
Ulfa choirunnisa <115141300+Chrnnsaulf@users.noreply.github.com>
2023-04-28 15:47:59 +0700

    Add files via upload

---
+++ pkginterface/src/pkginterface/Interface.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkginterface;
+
+/**
+ *
+ * @author lk1_siswa
+ */
+public class Interface {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+        myclass m = new myclass();
+        m.mymethod1();
+        m.mymethod2();
+        m.mymethod3();
+                
+    }
+    
+}
---
+++ pkginterface/src/pkginterface/myclass.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkginterface;
+
+/**
+ *
+ * @author lk1_siswa
+ */
+public class myclass implements myinterface1, myinterface2 {
+    @Override
+    public void mymethod1() {
+        System.out.println("hello world");
+    }
+    @Override
+    public void mymethod2() {
+        System.out.println("hello java");
+    }
+    @Override
+    public void mymethod3() {
+        System.out.println("hello indonesia");
+    }
+}
